7cd48aebc0972e46f5ae352c81b23d152f89b318
aakashownprep <aakashownprep@gmail.com>
2026-06-13 09:38:57 +0530

    Fix formatting in student registration output

--- student_registration.java
+++ student_registration.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class student_registration{
     public static void main(String[] args){
         Scanner s1 =new Scanner(System.in);
-        System.out.println("====STUDENT REGISTRATION FOR====");
+        System.out.println("====STUDENT REGISTRATION FORM====");
         System.out.print("Enter student name:");
         String name=s1.nextLine();
         System.out.println("enter student age:");
